3a75124707ff22ae6e39440bed76c7d78c274ba1
Eric K <eric.koehli@mail.utoronto.ca>
2024-06-22 14:31:33 -0400

    removed shuttle and getting ready for fly.io

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,17 @@ mod routes;
 mod state;
 mod templates;
 
-#[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
-    let blog_posts = handlers::blog::list_blogs(); // Read blog posts
+#[tokio::main]
+async fn main() {
+    // Initialize tracing
+    tracing_subscriber::fmt::init();
 
+    // Read blog posts at startup and save them in the app state
+    let blog_posts = handlers::blog::list_blogs();
     let state = state::AppState::new(blog_posts);
 
-    let router = router::init_router(state.clone());
-
-    Ok(router.into())
+    // initialize our app with some routes
+    let app = router::init_router(state.clone()); // app/router
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
